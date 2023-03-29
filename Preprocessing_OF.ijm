a420d5c297fe140b4d4c6a4eb138d974bf403b47
jeredastrat <118002210+jeredastrat@users.noreply.github.com>
2023-03-29 16:54:33 -0400

    Add files via upload

---
+++ Preprocessing_OF.ijm
@@ -0,0 +1,157 @@
+//Preprocessing macro, made by Dr. Lining JU, edited by Jered A. Stratton
+// Evolution of Boldness and Exploratory Behavior in Giant Mice from Gough Island
+// Behavioral Ecology and Sociobiology
+// Jered A. Stratton, Mark J. Nolte, and Bret A. Payseur
+// Laboratory of Genetics, University of Wisconsin - Madison, WI 53706
+// Corresponding Author: Jered A. Stratton
+// Email for Correspondence: jstratton2@wisc.edu
+//
+run("Close All");
+print("\\Clear");
+//Indicate the video files locations
+HomeDirectory=getDirectory("Choose Source Directory");
+list=getFileList(HomeDirectory);
+//Reset the background color
+run("Colors...", "background=white");
+//Setup the stepwise analysis 
+framestep=5000;
+framerate = 30; // Set this to fps of video
+tPrev = 0.03; // Set this to timestamp of first frame based on fps
+//Welcome message
+Msg="Start batch preprocessing...";
+print(Msg);
+
+for (k=0;k<list.length;k++)
+{
+	ix=indexOf(list[k],"empty");
+	if (ix>0)//Check whether this is a background video
+	{
+		Refname=list[k];
+		Expname= substring(Refname,0,ix-1)+".avi";
+//Setup the file paths
+Refpath=HomeDirectory+File.separator+Refname;
+Exppath=HomeDirectory+File.separator+Expname;
+Deletedpath=HomeDirectory+File.separator+substring(Expname,0,ix-10)+"Deleted_Frames"+".txt";
+Msg="Start processing "+Expname+"...";
+print(Msg);
+File.append("previous.deleted", Deletedpath);
+
+run("AVI...", "select=[&Exppath] use convert");
+framerange=nSlices;
+close();
+
+frameCount = 0;
+for(i=1;i<=framerange;i=i+framestep)
+{
+fstart=i;
+fend=minOf(i+framestep-1,framerange);
+if (fend == (framerange)) {
+	fend = 0;
+}
+m=1; //index of deleted frames - Jered S.
+//////////////////////////////////////////////////////////////////
+//Import the last image of background avi video
+run("AVI...", "select=[&Refpath] use convert");
+N=nSlices;
+close();
+run("AVI...", "select=[&Refpath] first=&N last=&N use convert");
+
+//Import the experiment avi video and select frame numbers
+run("AVI...", "select=[&Exppath] first=&fstart last=&fstart use convert");
+//Make a stack for alignment
+run("Concatenate...", "  title=[Concatenated Alignment] image1=&Expname image2=&Refname image3=[-- None --]");
+//Image binarization
+setOption("BlackBackground", false);
+run("Make Binary", "method=Minimum background=Light thresholded remaining black");
+//Align the experiment video with the background video
+//run("StackReg ", "transformation=[Rigid Body]");
+//Import the experiment avi video and select frame numbers
+run("AVI...", "select=[&Exppath] first=&fstart last=&fend use convert");
+print(toString(nSlices)+" Frames after import");
+//Alignment with the 1st image as the background 
+run("Concatenate...", "  title=[Concatenated Stacks] image1=[Concatenated Alignment] image2=[&Expname] image3=[-- None --]");
+run("Delete Slice");
+//Bin the videos
+run("Bin...", "x=2 y=2 z=1 bin=Average");
+//Automatically find the boundary edge based on the 1st image.
+doWand(160, 115, 90.0, "Legacy");
+run("Auto Threshold", "method=Minimum white stack");
+//Clear outer area and leave the arena and the mouse.
+run("Clear Outside", "stack");
+
+//Delete ROI
+run("Select None");
+//Remove the first slice
+run("Delete Slice");
+//Smooth the tracked object
+run("Minimum...", "radius=4  stack");
+
+j=1;
+while (j<nSlices)
+{
+	setSlice(j);
+label = getInfo("slice.label");
+label = substring(label, 0, lengthOf(label)-2);
+tCur = parseFloat(label);
+diff = round((tCur-tPrev)*framerate-1);
+// Find and record blank frames removed by ImageJ based on slice timestamps
+for (t = 0; t < diff; t++) {
+	File.append(toString(j+frameCount-1) + "." + toString(m), Deletedpath); // Add frame number to deleted - Jered S.
+	m++;
+	print("Blank frame recorded at time: " + toString(tPrev));
+}
+getRawStatistics(count, mean, min, max, std);
+	if(min==255)
+		{
+			File.append(toString(j+frameCount-1) + "." + toString(m), Deletedpath); // Add frame number to deleted - Jered S. 
+			m++;
+			run("Delete Slice");
+		}
+		else{
+		m=1; // Change deleted index to 1 - Jered S.
+		j++;
+		}
+		tPrev = tCur;
+	}
+setSlice(j);
+label = getInfo("slice.label");
+label = substring(label, 0, lengthOf(label)-2);
+tCur = parseFloat(label);
+diff = round((tCur-tPrev)*framerate-1);
+for (t = 0; t < diff; t++) {
+	File.append(toString(j+frameCount-1) + "." + toString(m), Deletedpath); // Add frame number to deleted - Jered S.
+	m++;
+	print("Blank frame recorded at time: " + toString(tPrev));
+}
+tPrev = tCur;
+getRawStatistics(count, mean, min, max, std);
+if(nSlices==1 && min==255){
+	print("0 Frames after deletions");
+	Msg=Expname+" Finished frames: "+ toString(fend)+"/"+toString(framerange);
+	print(Msg);
+	continue;
+}
+print(toString(nSlices)+" Frames after deletions");
+if (fend == 0) {
+	fend = framerange;
+}
+frameCount=frameCount+nSlices;
+//Run the tracking and save the result file
+dotIndex = indexOf(Expname, ".");
+Expbody = substring(Expname, 0, dotIndex)+"_"+toString(fstart)+"-"+toString(fend);
+SaveDatapath=HomeDirectory+File.separator+"TkResults_"+Expbody+".txt";
+call("MTrack2_.setProperty","showPathLengths","false"); 
+call("MTrack2_.setProperty","showLabels","false"); 
+call("MTrack2_.setProperty","showPositions","false"); 
+run("MTrack2 ", "minimum=20 maximum=1000 maximum_=10000 minimum_=5 save show save=[&SaveDatapath]");
+run("Close All");
+Msg=Expname+" Finished frames: "+ toString(fend)+"/"+toString(framerange);
+print(Msg);
+}
+Msg=Expname+" completed!\n";
+print(Msg);
+	}
+}
+//Print the final message
+Msg="All files in the directory are completed!\n";
+print(Msg);
---
+++ ld_traj_analysis.py
@@ -0,0 +1,313 @@
+__author__ = 'Jered Stratton'
+# Evolution of Boldness and Exploratory Behavior in Giant Mice from Gough Island
+# Behavioral Ecology and Sociobiology
+# Jered A. Stratton, Mark J. Nolte, and Bret A. Payseur
+# Laboratory of Genetics, University of Wisconsin - Madison, WI 53706
+# Corresponding Author: Jered A. Stratton
+# Email for Correspondence: jstratton2@wisc.edu
+#
+# Calculates amount of time spent in light chamber of light/dark box
+import sys
+import math
+import statistics
+import re
+
+trajFileName = sys.argv[1]  # First trajectory file to input
+calibrationFileName = sys.argv[2]
+deletedFileName = sys.argv[3]  # File of deleted frame numbers
+outFilePrefix = sys.argv[4]  # Prefix for output file
+frameTotal = int(sys.argv[5])  # Total number of frames in video
+
+outFileName = outFilePrefix + "_lightDarkData.csv"
+output = []
+frameStep = 5000  # Number of frames per file
+trajFrameCount = 1  # Current frame number for output
+frameIndex = 1  # Current frame number for loading files
+nullFrames = []  # Frames with more or less than one object detected
+nullCur = 0  # Index of last null frame detected
+nullBlockSize = 0
+trajFrames = []  # Data for combined traj file
+outFrames = []  # Data for output traj file with deleted frames
+xPrev = 0
+yPrev = 0
+frameCount = 0
+timeFactor = 1/30  # Number of seconds per frame
+reportInterval = 60/timeFactor  # Number of frames before reporting measurements again
+timePastRight = 0
+timePastCenter = 0
+timePastLeft = 0
+rightEntrances = 0
+centerEntrances = 0
+leftEntrances = 0
+rightLatency = 0
+centerLatency = 0
+leftLatency = 0
+pastRight = False
+pastCenter = False
+pastLeft = False
+
+# Shorthand for positions in calibration file
+centerX = 0
+centerY = 0
+BRCx = 0
+BRCy = 0
+BLCx = 0
+BLCy = 0
+TRCx = 0
+TRCy = 0
+TLCx = 0
+TLCy = 0
+
+# Read in the calibration file and store positions in dummy objects
+dummyX = []
+dummyY = []
+for i in range(1, 6):
+    xPositions = []
+    yPositions = []
+    calibrationFile = open(calibrationFileName, 'r')
+    calibrationFile.readline()
+    calibrationFile.readline()
+    for line in calibrationFile:
+        line = line.rstrip()
+        lineValues = line.split('\t')
+        xPositions.append(float(lineValues[(i*3)-2]))
+        yPositions.append(float(lineValues[(i*3)-1]))
+    dummyX.append(statistics.mean(xPositions))
+    dummyY.append(statistics.mean(yPositions))
+    calibrationFile.close()
+
+# Identify center
+centerX = statistics.median(dummyX)
+centerY = statistics.median(dummyY)
+dummyX.remove(statistics.median(dummyX))
+dummyY.remove(statistics.median(dummyY))
+
+# Identify corners based on position relative to center
+for i in range(0, 4):
+    xCur = dummyX[i]
+    yCur = dummyY[i]
+    if xCur > centerX and yCur > centerY:
+        BRCx = xCur
+        BRCy = yCur
+    elif xCur < centerX and yCur > centerY:
+        BLCx = xCur
+        BLCy = yCur
+    elif xCur > centerX and yCur < centerY:
+        TRCx = xCur
+        TRCy = yCur
+    elif xCur < centerX and yCur < centerY:
+        TLCx = xCur
+        TLCy = yCur
+
+rightDistance = math.sqrt((TRCx - BRCx) ** 2 + (TRCy - BRCy) ** 2)  # Distance between two right points
+leftDistance = math.sqrt((TLCx - BLCx) ** 2 + (TLCy - BLCy) ** 2)  # Distance between two left points
+centerRightDistance = abs((BRCy-TRCy)*centerX-(BRCx-TRCx)*centerY+(BRCx*TRCy)-(BRCy*TRCx))/rightDistance  # Distance from center to right threshold
+centerLeftDistance = abs((BLCy-TLCy)*centerX-(BLCx-TLCx)*centerY+(BLCx*TLCy)-(BLCy*TLCx))/leftDistance  # Distance from center to left threshold
+leftRightDistance = centerRightDistance + centerLeftDistance
+
+# Obtain prefix for traj file names
+trajFilePrefix = re.search('.*(?=\d+-\d+)', trajFileName)
+
+# Function for removing empty tracks
+def filterLineValues(lineValue):
+    if lineValue == ' ':
+        return False
+    else:
+        return True
+
+# Function for collecting data in each frame
+def collectFrameInfo(xCur, yCur):
+    global pastRight
+    global pastCenter
+    global pastLeft
+    global rightEntrances
+    global centerEntrances
+    global leftEntrances
+    global rightLatency
+    global centerLatency
+    global leftLatency
+    global timePastRight
+    global timePastCenter
+    global timePastLeft
+    global output
+    global frameCount
+    global frameTotal
+    mRight = abs((BRCy - TRCy) * xCur - (BRCx - TRCx) * yCur + (BRCx * TRCy) - (BRCy * TRCx)) / rightDistance
+    mLeft = abs((BLCy - TLCy) * xCur - (BLCx - TLCx) * yCur + (BLCx * TLCy) - (BLCy * TLCx)) / leftDistance
+    if mLeft > leftRightDistance:  # Mouse is on right of right threshold
+        pastRight = False
+        pastCenter = False
+        pastLeft = False
+    else:
+        if mRight <= mLeft:  # Mouse is between right and center thresholds
+            if not pastRight:
+                rightEntrances += 1
+            pastRight = True
+            if rightLatency == 0:
+                rightLatency = frameCount
+                print(str(rightLatency * timeFactor) + " seconds to cross right threshold for the first time")
+            pastCenter = False
+            pastLeft = False
+            timePastRight += 1
+        else:  # Mouse is left of center threshold
+            if mRight > leftRightDistance:  # Mouse is past left threshold
+                if not pastLeft:
+                    leftEntrances += 1
+                pastLeft = True
+                pastCenter = True
+                pastRight = True
+                if leftLatency == 0:
+                    leftLatency = frameCount
+                    print(str(leftLatency * timeFactor) + " seconds to cross left threshold for the first time")
+                    if centerLatency == 0:
+                        centerLatency = frameCount
+                        print(str(centerLatency * timeFactor) + " seconds to cross center threshold for the first time")
+                        rightLatency = frameCount
+                        print(str(rightLatency * timeFactor) + " seconds to cross right threshold for the first time")
+                timePastRight += 1
+                timePastCenter += 1
+                timePastLeft += 1
+            else:  # Mouse is between center and left thresholds
+                if not pastCenter:
+                    centerEntrances += 1
+                pastCenter = True
+                pastRight = True
+                pastLeft = False
+                if centerLatency == 0:
+                    centerLatency = frameCount
+                    print(str(centerLatency * timeFactor) + " seconds to cross center threshold for the first time")
+                    if rightLatency == 0:
+                        rightLatency = frameCount
+                        print(str(rightLatency * timeFactor) + " seconds to cross right threshold for the first time")
+                timePastRight += 1
+                timePastCenter += 1
+    if frameCount % reportInterval == 0:
+        output.append([(frameCount*timeFactor), (timePastRight*timeFactor), (timePastCenter*timeFactor), (timePastLeft*timeFactor), rightEntrances, centerEntrances, leftEntrances])
+
+
+# Combine trajectory files from Preprocessing.ijm output
+while frameIndex < frameTotal:
+    fend = min(frameIndex+frameStep-1, frameTotal)
+    trajFileName = trajFilePrefix.group(0) + str(frameIndex) + '-' + str(fend) + '.txt'
+    trajFile = open(trajFileName, 'r')
+    trajFile.readline()
+    trajFile.readline()
+    for line in trajFile:
+        line = line.rstrip()
+        lineValues = line.split('\t')
+        lineValues = list(filter(filterLineValues, lineValues))
+        if len(lineValues) > 3:
+            if lineValues[3] == '*':
+                lineValues.remove('*')
+        if len(lineValues) > 3:	
+            print("More than one object detected at frame " + str(trajFrameCount))
+            trajFrames.append([trajFrameCount, None, None])
+        elif len(lineValues) > 1:
+            trajFrames.append([trajFrameCount, float(lineValues[1]), float(lineValues[2])])
+        else:
+            print("Null frame detected at frame " + str(trajFrameCount))
+            trajFrames.append([trajFrameCount, None, None])
+        trajFrameCount += 1
+    frameIndex += frameStep
+    trajFile.close()
+
+# Record deleted frames
+outIndex = 1
+trajIndex = 0
+deletedFile = open(deletedFileName, 'r')
+deletedFile.readline()
+blockIndex = re.search('\d+(?=\.\d+)', deletedFile.readline())  # Frame number where block is incorporated
+while int(blockIndex.group(0)) == 0:
+    outFrames.append([outIndex, None, None])
+    blockIndex = re.search('\d+(?=\.\d+)', deletedFile.readline())
+deletedBlockSize = 1  # Number of frames in block
+for line in deletedFile:
+    deletedCur = re.search('\d+(?=\.\d+)', line)
+    # Add line to block of deleted Frames if matches blockIndex
+    if deletedCur.group(0) == blockIndex.group(0):
+        deletedBlockSize += 1
+    # If line is start of new block
+    else:
+        # Copy trajFrames to outFrames up to deletedBlock
+        while int(blockIndex.group(0)) >= trajIndex+1:
+            outFrames.append([outIndex, trajFrames[trajIndex][1], trajFrames[trajIndex][2]])
+            outIndex += 1
+            trajIndex += 1
+        for i in range(0, deletedBlockSize):
+            outFrames.append([outIndex, None, None])
+            outIndex += 1
+        blockIndex = deletedCur
+        deletedBlockSize = 1
+
+if blockIndex is not None:
+    while int(blockIndex.group(0)) >= trajIndex+1:
+        outFrames.append([outIndex, trajFrames[trajIndex][1], trajFrames[trajIndex][2]])
+        outIndex += 1
+        trajIndex += 1
+    for i in range(0, deletedBlockSize):
+        outFrames.append([outIndex, None, None])
+        outIndex += 1
+while len(trajFrames) >= trajIndex+1:
+    outFrames.append([outIndex, trajFrames[trajIndex][1], trajFrames[trajIndex][2]])
+    outIndex += 1
+    trajIndex += 1
+deletedFile.close()
+
+# Collect data from combined list of frames
+print("Analyzing video at " + str(1/timeFactor) + " frames per second")
+frameCount = 1
+nullBlockSize = 0
+xPrev = outFrames[0][1]
+yPrev = outFrames[0][2]
+for i in range(0, len(outFrames)):
+    xCur = outFrames[i][1]
+    yCur = outFrames[i][2]
+    # Add 1 to block of deleted Frames if blank frame found
+    if xCur is None:
+        nullBlockSize += 1
+    # If line is not deleted
+    else:
+        if xPrev is None:  # Deleted Block found at beginning of video
+            curLeft = abs((BLCy - TLCy) * xCur - (BLCx - TLCx) * yCur + (BLCx * TLCy) - (BLCy * TLCx)) / leftDistance
+            if curLeft <= leftRightDistance and nullBlockSize >= 5:
+                print("Warning: deleted block of size " + str(nullBlockSize) + " detected past right threshold at frame: " + str(frameCount))
+            for j in range(0, nullBlockSize):
+                collectFrameInfo(xCur, yCur)
+                frameCount += 1
+            xPrev = xCur
+            yPrev = yCur
+            nullBlockSize = 0
+        # Add coordinates to deleted block based on flanking frame coordinates and add to output
+        prevLeft = abs((BLCy - TLCy) * xPrev - (BLCx - TLCx) * yPrev + (BLCx * TLCy) - (BLCy * TLCx)) / leftDistance
+        curLeft = abs((BLCy - TLCy) * xCur - (BLCx - TLCx) * yCur + (BLCx * TLCy) - (BLCy * TLCx)) / leftDistance
+        if (prevLeft <= leftRightDistance and curLeft <= leftRightDistance) and nullBlockSize >= 5:
+            print("Warning: deleted block of size " + str(nullBlockSize) + " detected past right threshold at frame: " + str(frameCount))
+        if curLeft > prevLeft:
+            xPrev = xCur
+            yPrev = yCur
+        for j in range(0, nullBlockSize):
+            collectFrameInfo(xPrev, yPrev)
+            frameCount += 1
+        collectFrameInfo(xCur, yCur)
+        frameCount += 1
+        xPrev = xCur
+        yPrev = yCur
+        nullBlockSize = 0
+
+if xCur is None:
+    prevLeft = abs((BLCy - TLCy) * xPrev - (BLCx - TLCx) * yPrev + (BLCx * TLCy) - (BLCy * TLCx)) / leftDistance
+    if prevLeft <= leftRightDistance and nullBlockSize >= 5:
+        print("Warning: deleted block of size " + str(nullBlockSize) + " detected past right threshold at frame: " + str(frameCount))
+    for j in range(0, nullBlockSize):
+        collectFrameInfo(xPrev, yPrev)
+        frameCount += 1
+
+output.append([(frameCount*timeFactor), (timePastRight*timeFactor), (timePastCenter*timeFactor), (timePastLeft*timeFactor), rightEntrances, centerEntrances, leftEntrances])
+
+# Output data to csv file
+outFile = open(outFileName, "w")
+outFile.write("Time (seconds),Time Past Right Threshold (seconds), Time Past Center Threshold (seconds), Time Past Left Threshold (seconds), Number of Right Threshold Crosses, Number of Center Threshold Crosses, Number of Left Threshold Crosses\n")
+for i in range(0, len(output)):
+    outFile.write(",".join(str(x) for x in output[i]))
+    outFile.write("\n")
+outFile.close()
