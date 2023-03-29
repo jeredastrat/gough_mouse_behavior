4bdf1bddd968d08a98e3205a09df5a00d274605c
jeredastrat <118002210+jeredastrat@users.noreply.github.com>
2023-03-29 16:58:45 -0400

    Add files via upload

---
+++ Stratton_et_al_2021/calc_mobile_thresh.py
@@ -0,0 +1,67 @@
+__author__ = 'Jered Stratton'
+# Evolution of Boldness and Exploratory Behavior in Giant Mice from Gough Island
+# Behavioral Ecology and Sociobiology
+# Jered A. Stratton, Mark J. Nolte, and Bret A. Payseur
+# Laboratory of Genetics, University of Wisconsin - Madison, WI 53706
+# Corresponding Author: Jered A. Stratton
+# Email for Correspondence: jstratton2@wisc.edu
+#
+# Set Mobile threshold using method of Shoji et al.
+import sys
+import math
+import statistics
+import numpy
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
+
+base = importr('base')
+utils = importr('utils')
+inflection = importr('inflection')
+MASS = importr('MASS')
+dplyr = importr('dplyr')
+stats = importr('stats')
+
+def mobilityThreshold(trajFileName):
+    """Build dataframe of distances from trajectory file, and use it to compute and return a mobility threshold"""
+    frameCount = 1
+    distances = []
+    trajFile = open(trajFileName, 'r')
+    trajFile.readline()
+    trajFile.readline()
+    line = trajFile.readline()
+    line = line.rstrip()
+    lineValues = line.split('\t')
+    xPrev = float(lineValues[1])
+    yPrev = float(lineValues[2])
+    for line in trajFile:
+        line = line.rstrip()
+        lineValues = line.split('\t')
+        xCur = float(lineValues[1])
+        yCur = float(lineValues[2])
+        distanceCur = math.sqrt((xCur - xPrev) ** 2 + (yCur - yPrev) ** 2)
+        if distanceCur >= 25:
+            print("Warning: >25 pixels traveled at frame " + str(frameCount))
+        else:
+            distances.append(distanceCur)
+        frameCount += 1
+        xPrev = xCur
+        yPrev = yCur
+
+    trajFile.close()
+    distances = robjects.FloatVector(distances)
+    density = stats.density(distances)
+    densityX = list(density.rx2(1))
+    densityY = list(density.rx2(2))
+    sliceIndex = densityY.index(max(densityY))
+    densityX = densityX[sliceIndex:]
+    densityY = densityY[sliceIndex:]
+    sliceIndex = numpy.searchsorted(densityX, 5)
+    densityX = densityX[:sliceIndex]
+    densityY = densityY[:sliceIndex]
+    densityX = robjects.FloatVector(densityX)
+    densityY = robjects.FloatVector(densityY)
+    kneepoint = inflection.uik(densityX, densityY)
+    mobileThresh = float(kneepoint[0])
+
+    print("Mobile threshold set at " + str(mobileThresh) + " pixels")
+    return (mobileThresh, frameCount)
